61e007814d991ac4d1825d4e023a877bff90f424
Odewale Ifeoluwa <hargarpay@gmail.com>
2018-10-07 13:09:13 +0100

    Learning Elixir

---
+++ lib/cards.ex
@@ -0,0 +1,27 @@
+defmodule Cards do
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five", "King"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+    # Solution 1
+    # cards = for value <- values do
+    #   for suit <- suits do
+    #     "#{value} of #{suit}"
+    #   end
+    # end
+    # List.flatten(cards);
+
+    # Solution 2
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+  def contains?(deck, card) do
+    Enum.member?(deck, String.capitalize(card))
+  end
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+end
